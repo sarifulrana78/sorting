5670f54429dc6fe24d399ed7e6f4a40fd65af60a
Rana Bhai <sarifulislam536@gmail.com>
2026-02-04 00:59:24 +0600

    Sort an array problems and solution

--- sort.js
+++ sort.js
@@ -1,6 +1,8 @@
 /* const num= [2,3,5,15,6,7,8,44]
 const num */
 
-const numbers =[9,4,8,2,6,7,3]
-const numbers_asc=numbers.sort()
+const numbers =[9,14,8,22,61,17,43]
+const numbers_asc=numbers.sort(function(a,b){return a-b})
+const numbers_dsc=numbers.sort(function(a,b){return b-a})
 console.log(numbers_asc)
+console.log(numbers_dsc)
